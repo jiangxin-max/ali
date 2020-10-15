0b98ab12d1ba9b45e5217ef783f0e570a4e148da
jiangxin-max <1750912515@qq.com>
2020-10-15 08:44:45 +0800

    ali

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Top from './pages/Top';
-
+import Main from './pages/main';
+import MainBar from './pages/mainBar';
+import IndexBottom from './pages/bottom';
 export default function App () {
 
   return (<div>
     <Top /> 
+    <Main />
+    <MainBar />
+    <IndexBottom />
     </div>)
 }
