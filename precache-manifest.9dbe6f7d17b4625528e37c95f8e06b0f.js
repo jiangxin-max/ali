1fa45d348f6606c0ff7c8d6f02bae6df35fa6027
jiangxin-max <1750912515@qq.com>
2020-10-15 09:11:52 +0800

    Updates

---
+++ precache-manifest.9dbe6f7d17b4625528e37c95f8e06b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ab482b118cfa9cf3dcc0b1411cc31b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7993fd16fef8d30b4302",
+    "url": "/static/css/main.35f03173.chunk.css"
+  },
+  {
+    "revision": "4a996306236a16afe02e",
+    "url": "/static/js/2.dd581803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd581803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7993fd16fef8d30b4302",
+    "url": "/static/js/main.7dc69848.chunk.js"
+  },
+  {
+    "revision": "27763ce3a1918c502fb7",
+    "url": "/static/js/runtime-main.a5d7d421.js"
+  }
+]);
